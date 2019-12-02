c50013b240ff3b77e995abd9ce8d149fbd01643e
Rider Sargent <rider.sargent@gmail.com>
2019-12-02 17:33:50 +0000

    Modularized days and exercises.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     println!("{:?}", day_1_exercise_2(args));
 }
 
+
+/// --- Day 1, Exercise 2 ------------------------------------------------------
 fn day_1_exercise_2(args: Vec<String>) -> i32 {
     let filename = &args[1];
     let file = fs::File::open(filename).expect("Error reading file");
@@ -19,6 +21,37 @@ fn day_1_exercise_2(args: Vec<String>) -> i32 {
     for line in reader.lines() {
         let mass = line.unwrap().parse().unwrap();
 
+        let fuel = recursively_calculate_fuel_for(mass);
+
+        total_fuel += fuel;
+    }
+
+    total_fuel
+}
+
+fn recursively_calculate_fuel_for(n: i32) -> i32 {
+    let fuel_for_n = (n as f64 / 3.0_f64).trunc() as i32 - 2;
+
+    if fuel_for_n <= 0 {
+        return 0;
+    } else {
+        return fuel_for_n + recursively_calculate_fuel_for(fuel_for_n);
+    }
+}
+
+
+/// --- Day 1, Exercise 1 ------------------------------------------------------
+fn day_1_exercise_1(args: Vec<String>) -> i32 {
+    let filename = &args[1];
+    let file = fs::File::open(filename).expect("Error reading file");
+
+    let reader = BufReader::new(file);
+
+    let mut total_fuel = 0;
+
+    for line in reader.lines() {
+        let mass: i32 = line.unwrap().parse().unwrap();
+
         let fuel = calculate_fuel_for(mass);
 
         total_fuel += fuel;
@@ -28,11 +61,6 @@ fn day_1_exercise_2(args: Vec<String>) -> i32 {
 }
 
 fn calculate_fuel_for(n: i32) -> i32 {
-    let fuel_for_n = (n as f64 / 3.0_f64).trunc() as i32 - 2;
-
-    if fuel_for_n <= 0 {
-        return 0;
-    } else {
-        return fuel_for_n + calculate_fuel_for(fuel_for_n);
-    }
+    (n as f64 / 3.0_f64).trunc() as i32 - 2
 }
+
