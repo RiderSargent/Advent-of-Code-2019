47ace2385d7a6c2285c7489cc8912b953f4c4448
Rider Sargent <rider.sargent@gmail.com>
2019-12-02 15:01:53 +0000

    Day 1, exercise 1 complete.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use std::env;
 use std::fs;
+use std::io::{prelude::*, BufReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
     let filename = &args[1];
+    let file = fs::File::open(filename).expect("Error reading file");
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let reader = BufReader::new(file);
 
-    println!("{}", contents);
+    let mut total_fuel = 0;
+
+    for line in reader.lines() {
+        let mass: i32 = line.unwrap().parse().unwrap();
+
+        let fuel = calculate_fuel_for(mass);
+
+        total_fuel += fuel;
+    }
+
+    println!("{:?}", total_fuel);
+}
+
+fn calculate_fuel_for(n: i32) -> i32 {
+    (n as f64 / 3.0_f64).trunc() as i32 - 2
 }
