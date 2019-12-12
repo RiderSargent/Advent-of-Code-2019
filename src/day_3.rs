23f8a81cb3afcf7d8b64e7b5c442d81c750ecc1a
Rider Sargent <rider.sargent@gmail.com>
2019-12-12 18:21:37 +0000

    Reverted to (i32, i32) for points.

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use std::fs;
+use std::hash::{Hash, Hasher};
 use std::io::{prelude::*, BufReader};
 use std::iter::FromIterator;
 
@@ -20,30 +21,66 @@ pub fn exercise_1() {
         routes.push(route.to_owned());
     }
 
-    let r1_points = get_points(routes[0].to_owned());
-    let r2_points = get_points(routes[1].to_owned());
+    // distance: 6 -> to (-3, 3)
+    // path length: 30 -> to (-5, 6)
+    routes = vec![
+        vec!["R8", "U5", "L5", "D3"].into_iter().map(|d| d.to_string()).collect(),
+        vec!["U7", "R6", "D4", "L4"].into_iter().map(|d| d.to_string()).collect(),
+    ];
 
-    // println!("r1_points: {:?}", r1_points);
-    // println!("r2_points: {:?}", r2_points);
+    // // distance: 159
+    // // path length: 610
+    // routes = vec![
+    //     vec!["R75","D30","R83","U83","L12","D49","R71","U7","L72"].into_iter().map(|d| d.to_string()).collect(),
+    //     vec!["U62","R66","U55","R34","D71","R55","D58","R83"].into_iter().map(|d| d.to_string()).collect(),
+    // ];
 
-    let intersection = r1_points.intersection(&r2_points);
+    // // distance: 135
+    // // path length: 410
+    // routes = vec![
+    //     vec!["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"].into_iter().map(|d| d.to_string()).collect(),
+    //     vec!["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"].into_iter().map(|d| d.to_string()).collect(),
+    // ];
 
-    println!("intersection: {:?}", intersection);
+    let r1_points: Vec<(i32, i32)> = get_points(routes[0].to_owned());
+    let r2_points: Vec<(i32, i32)> = get_points(routes[1].to_owned());
 
-    let mut min_distance: i32 = std::i32::MAX;
-    for p in intersection {
-        if p.row != 0 || p.col != 0 {
-            if p.distance < min_distance {
-                min_distance = p.distance;
-            }
-        }
-    }
+    print_points("r1_points", &r1_points);
+    print_points("r2_points", &r2_points);
 
-    println!("[D3E1] distance: {:?}", min_distance);
+    // Need to clone points iter into HashSet
+    // HashSet::from_iter(points.iter().cloned())
+
+    // let intersection = r1_points.intersection(&r2_points);
+
+    // let min_distance: i32 = std::i32::MAX;
+
+    // let mut l1: usize;
+    // let mut l2: usize;
+    // let mut min_path_length: usize = std::usize::MAX;
+
+    // println!("\nintersection:");
+    // for p in intersection {
+    //     if p.row != 0 || p.col != 0 {
+    //         // println!("  {:?}", p);
+    //         // if p.distance < min_distance {
+    //         //     min_distance = p.distance;
+    //         // }
+    //         l1 = r1_points.into_iter().position(|q| q.row == p.row && q.col == p.col).unwrap();
+    //         l2 = r2_points.into_iter().position(|q| q.row == p.row && q.col == p.col).unwrap();
+    //         let combined_path_length = l1 + l2;
+    //         if combined_path_length < min_path_length {
+    //             min_path_length = combined_path_length;
+    //         }
+    //     }
+    // }
+    // println!("");
+
+    // println!("[D3E1] distance: {:?}", min_distance);
 }
 
-fn get_points(route: Vec<String>) -> HashSet<Point> {
-    let mut points: Vec<Point> = vec![Point::new(0, 0, 0)];
+fn get_points(route: Vec<String>) -> Vec<(i32, i32)> {
+    let mut points: Vec<(i32, i32)> = vec![(0, 0)];
 
     for line in route {
         let chars: Vec<char> = line.chars().collect();
@@ -54,76 +91,39 @@ fn get_points(route: Vec<String>) -> HashSet<Point> {
             .collect::<String>()
             .parse::<i32>()
             .unwrap();
-        let prev: Point = *points.last().unwrap();
+        let (r, c) = *points.last().unwrap();
 
         match direction {
             'U' => {
                 for i in 1..=distance {
-                    points.push(Point::new(
-                        prev.row - i,
-                        prev.col.to_owned(),
-                        prev.path_length + i,
-                    ));
+                    points.push((r - i, c));
                 }
             }
             'D' => {
                 for i in 1..=distance {
-                    points.push(Point::new(
-                        prev.row + i,
-                        prev.col.to_owned(),
-                        prev.path_length + i,
-                    ));
+                    points.push((r + i, c));
                 }
             }
             'L' => {
                 for i in 1..=distance {
-                    points.push(Point::new(
-                        prev.row.to_owned(),
-                        prev.col - i,
-                        prev.path_length + i,
-                    ));
+                    points.push((r, c - i,));
                 }
             }
             'R' => {
                 for i in 1..=distance {
-                    points.push(Point::new(
-                        prev.row.to_owned(),
-                        prev.col + i,
-                        prev.path_length + i,
-                    ));
+                    points.push((r, c + i));
                 }
             }
             _ => panic!("Bad data: Illegal direction."),
         }
     }
 
-    HashSet::from_iter(points.iter().cloned())
+    points
 }
 
-#[derive(Clone, Copy, Debug, Hash)]
-struct Point {
-    row: i32,
-    col: i32,
-    distance: i32,
-    path_length: i32,
-}
-
-impl Point {
-    pub fn new(row: i32, col: i32, path_length: i32) -> Self {
-        Point {
-            row: row,
-            col: col,
-            distance: row.abs() + col.abs(),
-            path_length: path_length,
-        }
+fn print_points(name: &str, points: &Vec<(i32, i32)>) {
+    println!("\n{}:", name);
+    for i in 0..points.len() {
+        println!("{:2}:  {:?}", i, points[i]);
     }
 }
-
-impl PartialEq for Point {
-    fn eq(&self, other: &Self) -> bool {
-        self.row == other.row && self.col == other.col
-    }
-}
-
-impl Eq for Point {}
-
