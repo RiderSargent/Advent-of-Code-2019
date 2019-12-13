7e2cd1e598c13f49bc5467e168a10a2051119c70
Rider Sargent <rider.sargent@gmail.com>
2019-12-13 17:53:11 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 fn main() {
     // day_1::exercise_1();
     // day_1::exercise_2();
     // day_2::exercise_1();
     // day_2::exercise_2();
-    day_3::exercise_1();
-    day_3::exercise_2();
+    // day_3::exercise_1();
+    // day_3::exercise_2();
+    day_4::exercise_1();
 }
