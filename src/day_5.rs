9fce63cd08679895c488c98dccfc9a383b88ba0a
Rider Sargent <rider.sargent@gmail.com>
2019-12-22 23:46:59 -0500

    Day 5 exercise 1 working. Needs cleanup.

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,12 +1,7 @@
 use crate::intcode;
 
 pub fn exercise_1() {
-    // let program = intcode::load_program("input_day_05.txt");
-    let program: Vec<i32> = vec![3, 0, 4, 0, 99];
+    let program = intcode::load_program("input_day_05.txt");
 
-    println!("{:?}", program);
-
-    let final_program = intcode::run_program(program);
-
-    println!("[D5E1]: {:?}", final_program);
+    intcode::run_program(program);
 }
--- src/intcode.rs
+++ src/intcode.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use std::io::prelude::*;
 
 pub fn load_program(filename: &str) -> Vec<i32> {
     let raw_program: String = fs::read_to_string(filename).expect("Error reading file");
@@ -19,42 +20,104 @@ pub fn run_program(mut program: Vec<i32>) -> Vec<i32> {
     let program_length = program.len();
 
     while i < program_length && program[i] != 99 {
+        let mut full_opcode: Vec<i32> = digitize(&program[i]);
+        let opcode: i32;
+        let mut parameter_modes: Vec<i32>;
         let offset: usize;
-        match program[i] {
-            1 => {
-                // add - opcode, v1, v2, store_index
-                let i_1: usize = program[i + 1] as usize;
-                let i_2: usize = program[i + 2] as usize;
+
+        // println!("program[i]: {:?}", program[i]);
+
+        // set opcode and parameter modes
+        if program[i] < 100 {
+            opcode = program[i];
+            parameter_modes = vec![];
+        } else {
+            opcode = full_opcode.pop().unwrap() + full_opcode.pop().unwrap() * 10;
+            parameter_modes = full_opcode;
+        }
+
+        // println!("parameter_modes: {:?}", parameter_modes);
+
+        match opcode {
+            01 => {
+                // ADD - opcode, read 1 index, read 2 index, write index
+                let value_1: i32;
+                let value_2: i32;
+
+                match parameter_modes.pop() {
+                    None | Some(0) => {
+                        let i_1: usize = program[i + 1] as usize;
+                        value_1 = program[i_1];
+                    }
+                    Some(1) => {
+                        value_1 = program[i + 1];
+                    }
+                    _ => panic!("Error")
+                }
+
+                match parameter_modes.pop() {
+                    None | Some(0) => {
+                        let i_2: usize = program[i + 2] as usize;
+                        value_2 = program[i_2];
+                    }
+                    Some(1) => {
+                        value_2 = program[i + 2];
+                    }
+                    _ => panic!("Error")
+                }
+
                 let store_index: usize = program[i + 3] as usize;
 
-                let value_1 = program[i_1];
-                let value_2 = program[i_2];
                 program[store_index] = value_1 + value_2;
                 offset = 4;
             }
-            2 => {
-                // multiply - opcode, v1, v2, store_index
-                let i_1: usize = program[i + 1] as usize;
-                let i_2: usize = program[i + 2] as usize;
+            02 => {
+                // MULTIPLY - opcode, read 1 index, read 2 index, write index
+                let value_1;
+                let value_2;
+
+                match parameter_modes.pop() {
+                    None | Some(0) => {
+                        let i_1: usize = program[i + 1] as usize;
+                        value_1 = program[i_1];
+                    }
+                    Some(1) => {
+                        value_1 = program[i + 1];
+                    }
+                    _ => panic!("Error")
+                }
+
+                match parameter_modes.pop() {
+                    None | Some(0) => {
+                        let i_2: usize = program[i + 2] as usize;
+                        value_2 = program[i_2];
+                    }
+                    Some(1) => {
+                        value_2 = program[i + 2];
+                    }
+                    _ => panic!("Error")
+                }
+
                 let store_index: usize = program[i + 3] as usize;
 
-                let value_1 = program[i_1];
-                let value_2 = program[i_2];
                 program[store_index] = value_1 * value_2;
                 offset = 4;
             }
-            3 => {
-                // save
+            03 => {
+                // INPUT - opcode, write index
                 // Opcode 3 takes a single integer as input and saves it to the
                 // position given by its only parameter. For example, the
                 // instruction 3,50 would take an input value and store it at
                 // address 50.
+                let i_1: usize = program[i + 1] as usize;
                 let mut input = String::new();
+
+                print!("Input: ");
+                io::stdout().flush().unwrap();
                 match io::stdin().read_line(&mut input) {
                     Ok(_) => {
                         match input.trim().parse::<i32>() {
                             Ok(value) => {
-                                let i_1: usize = program[i + 1] as usize;
                                 program[i_1] = value;
                             }
                             Err(error) => {
@@ -68,13 +131,25 @@ pub fn run_program(mut program: Vec<i32>) -> Vec<i32> {
                 }
                 offset = 2;
             }
-            4 => {
-                // output
+            04 => {
+                // OUTPUT - opcode, read index
                 // Opcode 4 outputs the value of its only parameter. For
                 // example, the instruction 4,50 would output the value at
                 // address 50.
-                let i_1: usize = program[i + 1] as usize;
-                println!("Output: {}", program[i_1]);
+                let value;
+
+                match parameter_modes.pop() {
+                    None | Some(0) => {
+                        let i_1: usize = program[i + 1] as usize;
+                        value = program[i_1];
+                    }
+                    Some(1) => {
+                        value = program[i + 1];
+                    }
+                    _ => panic!("Error")
+                }
+
+                println!("Output: {}", value);
                 offset = 2;
             }
             99 => {
@@ -86,4 +161,20 @@ pub fn run_program(mut program: Vec<i32>) -> Vec<i32> {
     }
 
     program
+}
+
+fn digitize(n: &i32) -> Vec<i32> {
+    let mut digits: Vec<i32> = Vec::new();
+    let mut remaining = n;
+    let mut next;
+
+    while remaining > &9 {
+        digits.push(remaining % 10);
+        next = remaining / 10;
+        remaining = &next;
+    }
+    digits.push(*remaining);
+    digits.reverse();
+
+    digits
 }
