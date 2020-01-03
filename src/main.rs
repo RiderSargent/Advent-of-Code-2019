7199c01e9fedacd0f444639bfddedd439d056180
Rider Sargent <rider.sargent@gmail.com>
2020-01-03 19:08:50 +0000

    Commented out println!s in main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,18 @@ mod intcode;
 mod tests;
 
 fn main() {
-    println!("[D1E1] Total fuel: {}", day_1::exercise_1());
-    println!("[D1E2] Total fuel: {}", day_1::exercise_2());
-    println!("[D2E1] Value at position 0: {:?}", day_2::exercise_1());
-    println!("[D2E2] 100 * noun + verb: {:?}", day_2::exercise_2());
-    println!("[D3E1] distance: {:?}", day_3::exercise_1());
-    println!("[D3E2] min_path_length: {:?}", day_3::exercise_2());
-    println!(
-        "[D4E1] number of potential passwords: {}",
-        day_4::exercise_1()
-    );
-    println!(
-        "[D4E2] number of potential passwords: {}",
-        day_4::exercise_2()
-    );
-    println!("[D5E1] program output: {:?}", day_5::exercise_1());
-    println!("[D5E2] program output: {:?}", day_5::exercise_2());
-    println!("[D6E1] Number of orbits: {}", day_6::exercise_1());
-    println!(
-        "[D6E2] shortest orbital transfer path length: {:?}",
-        day_6::exercise_2()
-    );
-    println!("[D7E1] max thruster signal: {:?}", day_7::exercise_1());
+    // println!("[D1E1] Total fuel: {}", day_1::exercise_1());
+    // println!("[D1E2] Total fuel: {}", day_1::exercise_2());
+    // println!("[D2E1] Value at position 0: {:?}", day_2::exercise_1());
+    // println!("[D2E2] 100 * noun + verb: {:?}", day_2::exercise_2());
+    // println!("[D3E1] distance: {:?}", day_3::exercise_1());
+    // println!("[D3E2] min_path_length: {:?}", day_3::exercise_2());
+    // println!("[D4E1] number of potential passwords: {}", day_4::exercise_1());
+    // println!("[D4E2] number of potential passwords: {}", day_4::exercise_2());
+    // println!("[D5E1] program output: {:?}", day_5::exercise_1());
+    // println!("[D5E2] program output: {:?}", day_5::exercise_2());
+    // println!("[D6E1] Number of orbits: {}", day_6::exercise_1());
+    // println!("[D6E2] shortest orbital transfer path length: {:?}", day_6::exercise_2());
+    // println!("[D7E1] max thruster signal: {:?}", day_7::exercise_1());
+    // println!("[D7E2] max thruster signal: {:?}", day_7::exercise_2());
 }
